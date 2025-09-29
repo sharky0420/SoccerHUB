623bf913e5ddcac2794c0b2db435084f93bdc12a
sharky0420 <59413198+sharky0420@users.noreply.github.com>
2025-09-29 15:22:55 +0200

    Convert PostCSS config to ESM

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+const config = {
+  plugins: [tailwindcss, autoprefixer],
 };
+
+export default config;
