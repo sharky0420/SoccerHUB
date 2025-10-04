dac37c6802371e3aee2f04a30689a7fd431f6a73
sharky0420 <59413198+sharky0420@users.noreply.github.com>
2025-10-04 14:15:22 +0200

    Handle missing Reanimated Babel plugin gracefully

--- ios/babel.config.js
+++ ios/babel.config.js
@@ -1,17 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = function (api) {
   api.cache(true);
+
+  const plugins = [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': './src'
+        }
+      }
+    ]
+  ];
+
+  const reanimatedPluginPath = path.join(
+    __dirname,
+    'node_modules',
+    'react-native-reanimated',
+    'plugin.js'
+  );
+
+  if (fs.existsSync(reanimatedPluginPath)) {
+    plugins.push(reanimatedPluginPath);
+  }
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-reanimated/plugin',
-      [
-        'module-resolver',
-        {
-          alias: {
-            '@': './src'
-          }
-        }
-      ]
-    ]
+    plugins
   };
 };
